7d1b6e72f82a9989379c545ee55b90d00c77def8
Alex Athanasopoulos <alex@melato.org>
2023-09-26 00:51:33 +0300

    ported cloudconfiglxd to use the incus codebase

---
+++ client/util.go
@@ -0,0 +1,90 @@
+package client
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/lxc/incus/shared"
+	config "melato.org/cloudconfigincus/cliconfig"
+)
+
+func PathExists(name string) bool {
+	_, err := os.Stat(name)
+	if err != nil && os.IsNotExist(err) {
+		return false
+	}
+
+	return true
+}
+
+/*
+UnixSocket returns the default LXD Unix Socket
+See https://github.com/canonical/lxd/blob/master/client/connection.go
+We use a similar strategy as connection.go ConnectLXDUnixWithContext(),
+except that we add /var/snap/lxd/common/lxd/ to the LXD directories searched.
+If no LXD directory is found, return "", nil.
+*/
+func UnixSocket() (string, error) {
+	path := os.Getenv("LXD_SOCKET")
+	if path != "" {
+		return path, nil
+	}
+	for _, socket := range []string{
+		"/var/snap/lxd/common/lxd/unix.socket",
+		"/var/lib/incus/unix.socket",
+	} {
+		if PathExists(socket) {
+			return socket, nil
+		}
+
+	}
+	return "", fmt.Errorf("no lxd or incus socket found")
+}
+
+/*
+ConfigDir returns the default LXD client configuration directory.
+See https://github.com/canonical/lxd/blob/master/client/main.go
+We use a similar strategy as main.go func (c *cmdGlobal) PreRun,
+except that we add $HOME/snap/lxd/common/config/ to the LXD config directories searched.
+If no LXD CONF directory is found, return "", nil.
+*/
+func ConfigDir() (string, error) {
+	configDir := os.Getenv("LXD_CONF")
+	if configDir != "" {
+		return configDir, nil
+	}
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	userConfigDir, err := os.UserConfigDir()
+	if err == nil && userConfigDir != "" {
+		configDir = filepath.Join(userConfigDir, "lxc")
+		if shared.PathExists(configDir) {
+			return configDir, nil
+		}
+	}
+	configDir = filepath.Join(home, "snap", "lxd", "common", "config")
+	if shared.PathExists(configDir) {
+		return configDir, nil
+	}
+	var c config.Config
+	configDir = c.GlobalConfigPath()
+	if shared.PathExists(configDir) {
+		return configDir, nil
+	}
+	return "", nil
+}
+
+func LoadConfig() (*config.Config, error) {
+	configDir, err := ConfigDir()
+	if err != nil {
+		return nil, err
+	}
+	if Trace {
+		fmt.Printf("using %s\n", configDir)
+	}
+	confPath := os.ExpandEnv(filepath.Join(configDir, "config.yml"))
+	return config.LoadConfig(confPath)
+}
---
+++ main/cloudconfig-incus.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"os"
+
+	lxd "github.com/lxc/incus/client"
+	"melato.org/cloudconfig"
+	"melato.org/cloudconfig/ostype"
+	"melato.org/cloudconfigincus"
+	"melato.org/cloudconfigincus/client"
+	"melato.org/command"
+	"melato.org/command/usage"
+)
+
+//go:embed version
+var version string
+
+//go:embed usage.yaml
+var usageData []byte
+
+type App struct {
+	client.LxdClient
+	Instance string `name:"i" usage:"LXD instance to configure"`
+	OS       string `name:"ostype" usage:"OS type"`
+	os       cloudconfig.OSType
+	server   lxd.InstanceServer
+}
+
+func (t *App) Configured() error {
+	if t.Instance == "" {
+		return fmt.Errorf("missing instance")
+	}
+	err := t.LxdClient.Configured()
+	if err != nil {
+		return err
+	}
+	server, err := t.CurrentServer()
+	if err != nil {
+		return err
+	}
+	switch t.OS {
+	case "":
+	case "alpine":
+		t.os = &ostype.Alpine{}
+	case "debian":
+		t.os = &ostype.Debian{}
+	default:
+		return fmt.Errorf("unknown OS type: %s", t.OS)
+	}
+	t.server = server
+	return nil
+}
+
+func (t *App) Apply(configFiles ...string) error {
+	base := cloudconfiglxd.NewInstanceConfigurer(t.server, t.Instance)
+	base.Log = os.Stdout
+	configurer := cloudconfig.NewConfigurer(base)
+	configurer.OS = t.os
+	configurer.Log = os.Stdout
+	if len(configFiles) == 1 && configFiles[0] == "-" {
+		return configurer.ApplyStdin()
+	} else {
+		return configurer.ApplyConfigFiles(configFiles...)
+	}
+}
+
+func (t *App) FileExists(path string) error {
+	base := cloudconfiglxd.NewInstanceConfigurer(t.server, t.Instance)
+	base.Log = os.Stdout
+	exists, err := base.FileExists(path)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%s: %v\n", path, exists)
+	return nil
+}
+
+func main() {
+	cmd := &command.SimpleCommand{}
+	var app App
+	cmd.Command("apply").Flags(&app).RunFunc(app.Apply)
+	cmd.Command("file-exists").Flags(&app).RunFunc(app.FileExists)
+	cmd.Command("version").RunFunc(func() { fmt.Println(version) })
+
+	usage.Apply(cmd, usageData)
+	command.Main(cmd)
+}
